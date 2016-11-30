17f2a40d27eb29e50346767e5ee814b941138283
EoinRaff <eoinrafferty@gmail.com>
2016-11-30 12:49:08 +0100

    tidied up UI

--- networking_Client/Network_Hand-in#7Client/Program.cs
+++ networking_Client/Network_Hand-in#7Client/Program.cs
@@ -6,23 +6,26 @@ using System.Collections.Generic;
 
 namespace TcpEchoClient
 {
-	class TcpEchoClient
-	{
+    class TcpEchoClient
+    {
         static public bool isJudge = false;
 
-		static void Main(string[] args)
-		{
-			string selection = "";
+        static void Main(string[] args)
+        {
+            string selection = "";
 
-			string username = "";
-			string ip = "";
-			int port;
+            string username = "";
+            string ip = "";
+            int port;
 
-			Console.WriteLine("Starting Strings against Medialogy game client...");
+            Console.WriteLine("Starting Strings Against Medialogy game client...");
 
-			Console.WriteLine ("Select your username");
-			username = Console.ReadLine ();
+            Console.WriteLine("Select your username");
+            username = Console.ReadLine();
 
+            /*
+             * outcommented for quick testing
+             * 
 			Console.WriteLine ("Enter server IP adress");
 			ip = Console.ReadLine ();
 
@@ -31,29 +34,30 @@ namespace TcpEchoClient
 			port = Int32.Parse (selection);
 
 			Console.WriteLine ("Connecting to server: " + ip + " on port: " + port);
+            */
 
+            TcpClient client = new TcpClient("192.168.43.29", 1234);
+            NetworkStream stream = client.GetStream();
+            StreamReader reader = new StreamReader(stream);
+            StreamWriter writer = new StreamWriter(stream) { AutoFlush = true };
 
-			TcpClient client = new TcpClient(ip, port);
-			NetworkStream stream = client.GetStream();
-			StreamReader reader = new StreamReader(stream);
-			StreamWriter writer = new StreamWriter(stream) { AutoFlush = true };
+            Console.Clear();
+            writer.WriteLine(username);
+            Console.WriteLine("Hello! Welcome to Strings Against Medialogy.");
 
-			Console.Clear ();
+            Console.Write("Press [p] to join game\n");
+            Console.Write("Press [x] to exit game\n");
 
-			Console.WriteLine ("Hello! Welcome to Strings Against Medialogy.");
+            string lineToSend = Console.ReadLine();
+            writer.WriteLine(lineToSend);
+            Console.Clear();
 
-			Console.Write("Press [p] to join game\n");
-			Console.Write("Press [x] to exit game\n");
+            while (true)
+            {
+                switch (lineToSend)
+                {
 
-			string lineToSend = Console.ReadLine();
-			writer.WriteLine(lineToSend);
-			Console.Clear ();
-
-			while (true)
-			{
-				switch (lineToSend) {
-
-				case "p":
+                    case "p":
 
                         ////////////////////////////////////////////////////
                         /*
@@ -78,37 +82,31 @@ namespace TcpEchoClient
                             string lineReceived = reader.ReadLine();
                             Console.WriteLine("Received from server: " + lineReceived);
                         }*/
-            ////////////////////////////////////////////////////	
-            //string lineReceived = reader.ReadLine ();
-            //Console.WriteLine (lineReceived);
-            //	Console.WriteLine (" ");
-            Console.WriteLine ("Your hand of strings have been dealt \n Choose the string you find the most suitable \n for the missing part in the following statement: \n \n");
+                        ////////////////////////////////////////////////////	
 
-					string lineReceived = reader.ReadLine();
-					lineReceived = reader.ReadLine();
-					List<string> yourHandOfCards = new List<string> (lineReceived.Split ('.'));
-					yourHandOfCards.ForEach (Console.WriteLine);
-					//lineToSend = Console.ReadLine();
-					//writer.WriteLine(lineToSend);
+                        Console.WriteLine("Your hand of strings have been dealt \n Choose the string you find the most suitable \n for the missing part in the following statement: \n \n");
 
-					Console.WriteLine ("Press [1] to choose the first statement");
-					Console.WriteLine ("Press [2] to choose the second"); 
-					Console.WriteLine ("Press [3] to choose the third"); 
-					Console.WriteLine ("Press [4] to choose the fourth");
-					Console.WriteLine ("Press [5] to choose the fifth"); 
+                        string lineReceived = reader.ReadLine();
+                        lineReceived = reader.ReadLine();
+                        List<string> yourHandOfCards = new List<string>(lineReceived.Split('.'));
+
+                        for (int i = 0; i < 5; i++)
+                        {
+                            Console.WriteLine("{0}: {1}", i + 1, yourHandOfCards[i]);
+                        }
+
+                        lineToSend = Console.ReadLine();
+
+                        writer.WriteLine(lineToSend);
 
 
-					lineToSend = Console.ReadLine();
-					writer.WriteLine(lineToSend);
+                        break;
+                    case "x":
 
-
-					break;
-				case "x":
-                        
-					break;
-				}   //end switch statement
-			}   //end while loop
-		}   //end main()
+                        break;
+                }   //end switch statement
+            }   //end while loop
+        }   //end main()
         static void displayAnswers()
         {
             //this should contain the code which will show the Judge the answers and let them vote
